d47673537fd0b8f4a9e49835717f63ea11b8c5cd
CHarris-VR <charrisagent@gmail.com>
2026-01-06 20:10:45 -0500

    first comm

---
+++ Walkthrough/starter.py
@@ -0,0 +1,14 @@
+#Week 4: Loops and Iterations
+# Goal: Identify internal vs external IP addresses
+
+ip_addresses = [
+    "192.168.1.25",
+    "10.0.0.8",
+    "172.16.5.14",
+    "8.8.8.8",
+    "172.15.3.2"
+]
+
+for ip in ip_addresses:
+    print (ip)
+
